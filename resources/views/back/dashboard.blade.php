0c3499f584c6182b6446162dda141d24380e75cb
budistwn15 <budi.setiawan112001@gmail.com>
2022-03-06 16:02:19 +0700

    update view dashboard

--- resources/views/back/dashboard.blade.php
+++ resources/views/back/dashboard.blade.php
@@ -2,5 +2,39 @@
 @section('title','Dashboard | Form Pembelian Urgent')
 
 @section('content')
-<h1>Hallo</h1>
+<div class="row">
+          <div class="col-lg-4 col-6">
+            <div class="small-box bg-info">
+              <div class="inner">
+                <h3>12</h3>
+                <p>Daily Request</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-stats-bars"></i>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 col-6">
+            <div class="small-box bg-success">
+              <div class="inner">
+                <h3>220</h3>
+                <p>Monthly Request</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-stats-bars"></i>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 col-6">
+            <div class="small-box bg-warning">
+              <div class="inner">
+                <h3>1350</h3>
+                <p>Yearly Request</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-stats-bars"></i>
+              </div>
+            </div>
+          </div>
+        </div>
 @endsection
