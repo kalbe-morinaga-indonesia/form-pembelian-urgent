11a29c2c98dc355796a68e3cc8e3bb452342bf22
budistwn15 <budi.setiawan112001@gmail.com>
2022-03-06 16:15:42 +0700

    update migration user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,13 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('musers', function (Blueprint $table) {
+            $table->id('intUserId');
+            $table->string('txtNama');
+            $table->string('txtNoHp');
+            $table->string('txtTempatLahir');
+            $table->date('dtmTanggalLahir');
+            $table->string('txtUsername')->unique();
+            $table->string('txtPassword');
+            $table->text('txtAlamat');
             $table->timestamps();
         });
     }
