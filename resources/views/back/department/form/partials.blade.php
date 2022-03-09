92116df70e2703ae10be2b07b35ff6187f06c3a5
budistwn15 <budi.setiawan112001@gmail.com>
2022-03-09 20:06:35 +0700

    update partial department

--- resources/views/back/department/form/partials.blade.php
+++ resources/views/back/department/form/partials.blade.php
@@ -1,6 +1,9 @@
     <div class="form-group">
         <label for="txtNamaDept">Nama Department</label>
-        <input type="text" class="form-control" name="txtNamaDept" id="txtNamaDept"
-            placeholder="Masukkan Nama Department">
+        <input type="text" class="form-control @error('txtNamaDept') is-invalid  @enderror" name="txtNamaDept" id="txtNamaDept"
+            placeholder="Masukkan Nama Department" value="{{ old('txtNamaDept') ?? $department->txtNamaDept }}">
+        @error('txtNamaDept')
+            <div class="text-danger">{{ $message }}</div>
+        @enderror
     </div>
     <button class="btn btn-primary" type="submit">{{ $button }}</button>
