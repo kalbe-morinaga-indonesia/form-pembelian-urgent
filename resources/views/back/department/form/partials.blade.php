5bf53e5c4087d1762cba5f8becea61430851fead
budistwn15 <budi.setiawan112001@gmail.com>
2022-03-09 19:50:03 +0700

    add file partial form department

---
+++ resources/views/back/department/form/partials.blade.php
@@ -0,0 +1,6 @@
+    <div class="form-group">
+        <label for="txtNamaDept">Nama Department</label>
+        <input type="text" class="form-control" name="txtNamaDept" id="txtNamaDept"
+            placeholder="Masukkan Nama Department">
+    </div>
+    <button class="btn btn-primary" type="submit">{{ $button }}</button>
