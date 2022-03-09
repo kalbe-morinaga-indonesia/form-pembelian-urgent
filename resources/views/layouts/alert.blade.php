7df4df97122ba99f7d5e07ed2b720f3a5a0dff9b
budistwn15 <budi.setiawan112001@gmail.com>
2022-03-09 21:05:47 +0700

    delete alert

--- resources/views/layouts/alert.blade.php
+++
@@ -1,8 +0,0 @@
-@if (session()->has('message'))
-<div class="alert alert-success" role="alert">
-    <span class="alert-inner--icon"><i class="fe fe-thumbs-up"></i></span>
-    <span class="alert-inner--text"><strong>Success!</strong>
-        {{$slot}}
-    </span>
-</div>
-@endif
