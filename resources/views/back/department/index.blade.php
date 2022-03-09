fd66e33bc8d98386bb23f9eefd1bfa242dfeb97a
budistwn15 <budi.setiawan112001@gmail.com>
2022-03-09 19:49:54 +0700

    update view index department

--- resources/views/back/department/index.blade.php
+++ resources/views/back/department/index.blade.php
@@ -2,5 +2,48 @@
 @section('title','Department | Form Pembelian Urgent')
 
 @section('content')
+<div class="row">
+    <div class="col-12">
+        @component('layouts.alert')
+        {{ session()->get('message') }}
+        @endcomponent
+        <div class="card">
+            <div class="card-header">
+                <div class="d-flex justify-content-between">
+                    <h3 class="card-title">Data Department</h3>
+                    <a href="{{ route('departments.create') }}" class="btn btn-primary">Tambah
+                        Department</a>
+                </div>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Nama Departemen</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($departments as $department)
+                            <tr>
+                                <td>{{ $loop->iteration }}</td>
+                                <td>{{ $department->txtNamaDept }}</td>
+                                <td>
+                                    <div class="btn-group">
+                                        <a href="#" class="btn btn-warning">Edit</a>
+                                        <a href="#" class="btn btn-danger">Delete</a>
+                                    </div>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 @endsection
