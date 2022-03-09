1782ea0661698d6c93827d40628402b68125dbc1
budistwn15 <budi.setiawan112001@gmail.com>
2022-03-09 13:02:49 +0700

    Hapus Component

--- resources/views/components/form/input.blade.php
+++
@@ -1,6 +0,0 @@
-<input type="{{ $type }}" name="{{ $name }}" id="{{ $id }}" class="form-control @error($name) is-invalid @enderror" @if ($value !== null && $value !== "") value="{{ $value }}" @else value="{{ old($name) }}"  @endif placeholder="{{ $placeholder }}" {{ $isReadOnly }}>
-<div class="text-danger">
-    @error($name)
-        $message
-    @enderror
-</div>
