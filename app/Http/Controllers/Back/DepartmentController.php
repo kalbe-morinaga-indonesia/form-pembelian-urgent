2f8457bebcb6a020139cea5552eb792fb7d55b40
budistwn15 <budi.setiawan112001@gmail.com>
2022-03-09 19:49:08 +0700

    Menambahkan method create

--- app/Http/Controllers/Back/DepartmentController.php
+++ app/Http/Controllers/Back/DepartmentController.php
@@ -3,14 +3,37 @@
 namespace App\Http\Controllers\Back;
 
 use App\Http\Controllers\Controller;
+use App\Models\Department;
 use Illuminate\Http\Request;
 
 class DepartmentController extends Controller
 {
     public function index()
     {
+        $departments = Department::get();
         return view('back.department.index', [
+            'title' => 'Department',
+            'departments' => $departments
+        ]);
+    }
+
+    public function create()
+    {
+        return view('back.department.create', [
             'title' => 'Department'
         ]);
+    }
+
+    public function store(Request $request)
+    {
+        $validateData = $request->validate([
+            'txtNamaDept' => 'required'
+        ], [
+            'txtNamaDept.required' => 'Nama Department wajib diisi'
+        ]);
+
+        Department::create($validateData);
+
+        return redirect()->route('departments.index')->with('message', 'Data Department Berhasil Ditambahkan');
     }
 }
