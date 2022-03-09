6c38004eb2d90f0e759836ccef7d605b0975250f
budistwn15 <budi.setiawan112001@gmail.com>
2022-03-09 12:44:19 +0700

    update controller department

--- app/Http/Controllers/Back/DepartmentController.php
+++ app/Http/Controllers/Back/DepartmentController.php
@@ -7,5 +7,10 @@ use Illuminate\Http\Request;
 
 class DepartmentController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('back.department.index', [
+            'title' => 'Department'
+        ]);
+    }
 }
