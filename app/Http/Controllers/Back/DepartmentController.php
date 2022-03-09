772d5a25d271d1dffec1651e4da3876a7ae1762f
budistwn15 <budi.setiawan112001@gmail.com>
2022-03-09 20:06:10 +0700

    Update controller department

--- app/Http/Controllers/Back/DepartmentController.php
+++ app/Http/Controllers/Back/DepartmentController.php
@@ -17,10 +17,11 @@ class DepartmentController extends Controller
         ]);
     }
 
-    public function create()
+    public function create(Department $department)
     {
         return view('back.department.create', [
-            'title' => 'Department'
+            'title' => 'Department',
+            'department' => $department
         ]);
     }
 
@@ -34,6 +35,36 @@ class DepartmentController extends Controller
 
         Department::create($validateData);
 
-        return redirect()->route('departments.index')->with('message', 'Data Department Berhasil Ditambahkan');
+        return redirect()->route('departments.index')
+            ->with('message', "Data Department Berhasil Ditambahkan");
+    }
+
+    public function edit(Department $department)
+    {
+        return view('back.department.edit', [
+            'title' => 'Department',
+            'department' => $department
+        ]);
+    }
+
+    public function update(Department $department, Request $request)
+    {
+        $validateData = $request->validate([
+            'txtNamaDept' => 'required'
+        ], [
+            'txtNamaDept.required' => 'Nama Department wajib diisi'
+        ]);
+
+        Department::where('id', $department->id)->update($validateData);
+
+        return redirect()->route('departments.index')
+            ->with('message', "Data Department Berhasil Diubah");
+    }
+
+    public function destroy(Department $department)
+    {
+        $department->delete();
+        return redirect()->route('departments.index')
+            ->with('message', "Data Department Berhasil Dihapus");
     }
 }
