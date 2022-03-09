aaec8b3e1d2854bf1009fae0f55dccdde1ea765a
budistwn15 <budi.setiawan112001@gmail.com>
2022-03-09 20:06:50 +0700

    update layout back

--- resources/views/layouts/back.blade.php
+++ resources/views/layouts/back.blade.php
@@ -43,7 +43,7 @@
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
+              <li class="breadcrumb-item"><a href="{{ route('dashboard') }}">Dashboard</a></li>
               <li class="breadcrumb-item active">{{ $title }}</li>
             </ol>
           </div>
