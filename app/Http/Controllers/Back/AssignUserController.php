d2d8f70b33ebe7eba24cd8c8c50f4a1bb844dfa6
budistwn15 <budi.setiawan112001@gmail.com>
2022-03-11 01:22:32 +0700

    update assign user controller

--- app/Http/Controllers/Back/AssignUserController.php
+++ app/Http/Controllers/Back/AssignUserController.php
@@ -2,10 +2,65 @@
 
 namespace App\Http\Controllers\Back;
 
-use App\Http\Controllers\Controller;
+use Spatie\Permission\Models\Role;
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use RealRashid\SweetAlert\Facades\Alert;
 
 class AssignUserController extends Controller
 {
-    //
+    public function index()
+    {
+        $users = User::has('roles')->get();
+        return view('back.role-and-permissions.assign-user.index', [
+            'title' => 'Assign User',
+            'users' => $users
+        ]);
+    }
+
+    public function create(User $user)
+    {
+        $roles = Role::get();
+        $users = User::get();
+        return view('back.role-and-permissions.assign-user.create', [
+            'title' => 'Assign User',
+            'roles' => $roles,
+            'user' => $user,
+            'users' => $users
+        ]);
+    }
+
+    public function store()
+    {
+        request()->validate([
+            'txtUsername' => 'required',
+            'roles' => 'array|required'
+        ]);
+
+        $user = User::where('txtUsername', request('txtUsername'))->first();
+        $user->assignRole(request('roles'));
+
+        Alert::success("Berhasil", "Role telah ditetapkan ke pengguna $user->name");
+        return redirect()->route('assign-users.index');
+    }
+
+    public function edit(User $user)
+    {
+        $roles = Role::get();
+        return view('back.role-and-permissions.assign-user.edit', [
+            'title' => 'Assign User',
+            'user' => $user,
+            'roles' => $roles
+        ]);
+    }
+
+    public function update(User $user)
+    {
+        $user->syncRoles(request('roles'));
+
+        Alert::success("Berhasil", "Rule telah disinkronkan ke {$user->txtNama}");
+
+        return redirect()->route('assign-users.index');
+    }
 }
