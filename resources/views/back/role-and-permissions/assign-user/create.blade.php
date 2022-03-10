02d1ae9858f7a131fecc0297747732ff438a3656
budistwn15 <budi.setiawan112001@gmail.com>
2022-03-11 01:23:21 +0700

    add file create

---
+++ resources/views/back/role-and-permissions/assign-user/create.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.back')
+@section('title','Assign User | Form Pembelian Urgent')
+
+@section('content')
+<div class="row">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">Assign User</h3>
+            </div>
+            <div class="card-body">
+                <form action="{{ route('assign-users.store') }}" method="POST">
+                @csrf
+                @include('back.role-and-permissions.assign-user.form',['button' => 'Assign'])
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
