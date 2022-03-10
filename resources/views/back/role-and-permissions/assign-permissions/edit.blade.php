a025d794d8bd107fe6422ecbd58ca2cf27ab522f
budistwn15 <budi.setiawan112001@gmail.com>
2022-03-11 01:23:00 +0700

    add file edit

---
+++ resources/views/back/role-and-permissions/assign-permissions/edit.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.back')
+@section('title','Assign Permission | Form Pembelian Urgent')
+
+@section('content')
+<div class="row">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">Assign Permission</h3>
+            </div>
+            <div class="card-body">
+                <form action="{{ route('assign-permissions.update',['role' => $role->id]) }}" method="POST">
+                @method('put')
+                @csrf
+                @include('back.role-and-permissions.assign-permissions.form',['button' => 'Sync'])
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
