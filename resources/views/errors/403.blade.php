dfbb968b81690a190fe976798ccffa77b59f6935
budistwn15 <budi.setiawan112001@gmail.com>
2022-03-14 10:10:25 +0700

    create file error 403

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>403 - Form Pembelian Urgent</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+
+    <div class="container">
+        <div class="row mt-5">
+            <div class="col-md-12 text-center">
+                <img src="{{ asset('theme/dist/img/403.jpg') }}" alt="404 Not Found" class="img-fluid d-block m-auto" width="600">
+                <h2>403 FORBIDDEN</h2>
+                <p class="lead">USER DOES NOT HAVE THE RIGHT ROLES.</p>
+                <a href="{{ route('dashboard') }}" class="btn btn-outline-dark rounded-pill p-2">BACK TO DASHBOARD</a>
+            </div>
+        </div>
+    </div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
