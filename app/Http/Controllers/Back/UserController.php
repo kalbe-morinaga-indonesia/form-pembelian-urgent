7b6b9f75db710a262767790720d6aa8b127c3b4a
budistwn15 <budi.setiawan112001@gmail.com>
2022-03-14 14:38:09 +0700

    Add controller user

--- app/Http/Controllers/Back/UserController.php
+++ app/Http/Controllers/Back/UserController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers\Back;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\UserRequest;
+use App\Models\Department;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use RealRashid\SweetAlert\Facades\Alert;
 
 class UserController extends Controller
 {
@@ -21,5 +25,65 @@ class UserController extends Controller
             'title' => 'Users',
             'users' => $users
         ]);
+    }
+
+    public function create(User $user)
+    {
+        $departments = Department::get();
+        return view('back.users.create', [
+            'title' => 'Users',
+            'user' => $user,
+            'departments' => $departments
+        ]);
+    }
+
+    public function store(UserRequest $request)
+    {
+        User::create([
+            'txtNama' =>  $request->txtNama,
+            'txtNoHp' =>  $request->txtNoHp,
+            'txtTempatLahir' =>  $request->txtTempatLahir,
+            'dtmTanggalLahir' =>  $request->dtmTanggalLahir,
+            'txtUsername' =>  $request->txtUsername,
+            'txtPassword' =>  Hash::make($request->txtPassword),
+            'txtAlamat' =>  $request->txtAlamat,
+            'mdepartment_id' =>  $request->mdepartment_id,
+        ]);
+
+        Alert::success("Berhasil", "Data user $request->txtNama berhasil ditambahkan");
+        return redirect()->route('users.index');
+    }
+
+    public function edit(User $user)
+    {
+        $departments = Department::get();
+        return view('back.users.edit', [
+            'title' => 'Users',
+            'user' => $user,
+            'departments' => $departments
+        ]);
+    }
+
+    public function update(UserRequest $request, User $user)
+    {
+        User::where('id', $user->id)->update([
+            'txtNama' =>  $request->txtNama,
+            'txtNoHp' =>  $request->txtNoHp,
+            'txtTempatLahir' =>  $request->txtTempatLahir,
+            'dtmTanggalLahir' =>  $request->dtmTanggalLahir,
+            'txtUsername' =>  $request->txtUsername,
+            'txtAlamat' =>  $request->txtAlamat,
+            'mdepartment_id' =>  $request->mdepartment_id,
+        ]);
+
+        Alert::success("Berhasil", "Data user $request->txtNama berhasil diubah");
+        return redirect()->route('users.index');
+    }
+
+    public function destroy(User $user)
+    {
+        $user->delete();
+        Alert::success("Berhasil", "Data user $user->txtNama berhasil diubah");
+        return redirect()->route('users.index');
     }
 }
