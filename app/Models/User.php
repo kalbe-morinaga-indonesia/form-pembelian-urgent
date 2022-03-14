2f8da288b91a2ecc4d409f151770055f9f0675b7
budistwn15 <budi.setiawan112001@gmail.com>
2022-03-14 09:02:07 +0700

    add relation

--- app/Models/User.php
+++ app/Models/User.php
@@ -52,4 +52,9 @@ class User extends Authenticatable
     {
         return $this->txtPassword;
     }
+
+    public function mdepartment()
+    {
+        return $this->belongsTo("App\Models\Department");
+    }
 }
