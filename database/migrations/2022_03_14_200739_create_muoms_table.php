6653d14a37ee4dcdfe8b33c0932f3bc8d0b26be6
budistwn15 <budi.setiawan112001@gmail.com>
2022-03-16 22:09:01 +0700

    add migration uom

---
+++ database/migrations/2022_03_14_200739_create_muoms_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMuomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('muoms', function (Blueprint $table) {
+            $table->id();
+            $table->string('txtItemCode');
+            $table->date('dtmTanggalKebutuhan');
+            $table->integer('intJumlahKebutuhan');
+            $table->string('txtInsertedBy')->nullable();
+            $table->string('txtUpdatedBy')->nullable();
+            $table->dateTime('dtmInsertedBy');
+            $table->dateTime('dtmUpdatedBy');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('muoms');
+    }
+}
