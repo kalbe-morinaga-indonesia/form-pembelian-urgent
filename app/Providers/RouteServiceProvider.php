175a97fed9f884c82c7e3c51fd83ed24ee2d4482
budistwn15 <budi.setiawan112001@gmail.com>
2022-03-16 22:08:38 +0700

    edit route service provider

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -18,7 +18,7 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     // public const HOME = '/home';
-    public const HOME = '/back/dashboard';
+    public const HOME = '/dashboard';
 
     /**
      * The controller namespace for the application.
