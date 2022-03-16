3dd1a5fd4220e2c38c2d7f1c347e77141de0f1c0
budistwn15 <budi.setiawan112001@gmail.com>
2022-03-17 01:17:17 +0700

    update model uom

--- app/Models/Uom.php
+++ app/Models/Uom.php
@@ -10,7 +10,7 @@ class Uom extends Model
     use HasFactory;
 
     protected $table = "muoms";
-    protected $fillable = ['txtItemCode'];
+    protected $guarded = [];
 
     const CREATED_AT = "dtmInsertedBy";
     const UPDATED_AT = "dtmUpdatedBy";
