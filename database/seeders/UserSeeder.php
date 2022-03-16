f2e2f80418c624edbdc76a3f0bc9ccb96f5b03cb
budistwn15 <budi.setiawan112001@gmail.com>
2022-03-16 22:09:07 +0700

    add user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\PermissionRegistrar;
 
 class UserSeeder extends Seeder
 {
@@ -13,6 +16,29 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(5)->create();
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+
+        // create permissions
+
+        // department
+        Permission::create(['name' => 'view_department']);
+        Permission::create(['name' => 'add_department']);
+        Permission::create(['name' => 'edit_department']);
+        Permission::create(['name' => 'delete_department']);
+
+        // create role
+        $role1 = Role::create(['name' => 'admin']);
+        $role1->givePermissionTo('view_department');
+        $role1->givePermissionTo('add_department');
+        $role1->givePermissionTo('edit_department');
+        $role1->givePermissionTo('delete_department');
+
+        $role2 = Role::create(['name' => 'user']);
+        $role3 = Role::create(['name' => 'dept_head']);
+        $role4 = Role::create(['name' => 'buyer']);
+        $role5 = Role::create(['name' => 'pu_svp']);
+
+        $user = \App\Models\User::factory()->create();
+        $user->assignRole($role1);
     }
 }
