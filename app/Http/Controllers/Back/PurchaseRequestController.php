d34e1c9b87d23ed4f082ae7c464c08e4d88cd7e7
budistwn15 <budi.setiawan112001@gmail.com>
2022-03-21 19:46:03 +0700

    update controller purchase request

--- app/Http/Controllers/Back/PurchaseRequestController.php
+++ app/Http/Controllers/Back/PurchaseRequestController.php
@@ -3,11 +3,22 @@
 namespace App\Http\Controllers\Back;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\PurchaseRequest;
+use App\Models\Barang;
 use App\Models\Department;
+use App\Models\Purchase;
 use Illuminate\Http\Request;
 
 class PurchaseRequestController extends Controller
 {
+
+    public function index()
+    {
+        return view('back.purchase.index', [
+            'title' => 'List Request'
+        ]);
+    }
+
     public function create()
     {
         $departments = Department::get();
@@ -15,5 +26,43 @@ class PurchaseRequestController extends Controller
             'title' => 'Purchase Request',
             'departments' => $departments
         ]);
+    }
+
+    public function store(PurchaseRequest $request)
+    {
+
+        if ($request->hasfile('txtFile')) {
+            foreach ($request->file('txtFile') as $file) {
+                $name = $file->getClientOriginalName();
+                $file->move(public_path() . '/files/', $name);
+                $fileData[] = $name;
+            }
+
+            $fileModal = new Purchase();
+            $fileModal->muser_id = $request->muser_id;
+            $fileModal->mdepartment_id = $request->mdepartment_id;
+            $fileModal->txtNoPurchaseRequest = $request->txtNoPurchaseRequest;
+            $fileModal->dtmDateCreated = $request->dtmDateCreated;
+            $fileModal->dtmDateRequired = $request->dtmDateRequired;
+            $fileModal->txtFile = json_encode($fileData);
+            $fileModal->txtReason = $request->txtReason;
+            $fileModal->status = "in process";
+
+            $fileModal->save();
+
+            $purchase_id = Purchase::where('txtNoPurchaseRequest', $request->txtNoPurchaseRequest)->first();
+
+            foreach ($request->barang as $key => $value) {
+                $dataBarang = new Barang();
+                $dataBarang->mpurchase_id = $purchase_id->id;
+                $dataBarang->txtNamaBarang = $value['txtNamaBarang'];
+                $dataBarang->intJumlah = $value['intJumlah'];
+                $dataBarang->txtSatuan = $value['txtSatuan'];
+                $dataBarang->txtKeterangan = $value['txtKeterangan'];
+                $dataBarang->save();
+            }
+
+            return back();
+        }
     }
 }
