e93e6e02db8e106be2f2f548219e1d6ca72ba187
budistwn15 <budi.setiawan112001@gmail.com>
2022-03-24 22:41:53 +0700

    update report controller

--- app/Http/Controllers/Back/ReportController.php
+++ app/Http/Controllers/Back/ReportController.php
@@ -2,17 +2,27 @@
 
 namespace App\Http\Controllers\Back;
 
-use App\Http\Controllers\Controller;
 use App\Models\Purchase;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class ReportController extends Controller
 {
     public function index()
     {
-        $approve = Purchase::where('status', 'approved by dept head')->get();
-        $not_approve = Purchase::where('status', 'rejected by dept head')->get();
-        $need_to_approve = Purchase::where('status', 'in process')->get();
+        $approve = Purchase::where([
+            ['muser_id', Auth::user()->id],
+            ['status', 'approved by dept head']
+        ])->get();
+        $not_approve = Purchase::where([
+            ['muser_id', Auth::user()->id],
+            ['status', 'rejected by dept head']
+        ])->get();
+        $need_to_approve = Purchase::where([
+            ['muser_id', Auth::user()->id],
+            ['status', 'in process']
+        ])->get();
         return view('back.report.index', [
             'title' => 'Report',
             'approve' => $approve,
