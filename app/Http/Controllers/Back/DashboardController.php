82802a6187a49c666b108ccc61cd569a55fd7e3b
budistwn15 <budi.setiawan112001@gmail.com>
2022-03-24 22:41:28 +0700

    update count dashboard

--- app/Http/Controllers/Back/DashboardController.php
+++ app/Http/Controllers/Back/DashboardController.php
@@ -3,14 +3,23 @@
 namespace App\Http\Controllers\Back;
 
 use App\Http\Controllers\Controller;
+use App\Models\Purchase;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class DashboardController extends Controller
 {
     public function index()
     {
+        $today = Carbon::now();
+        $daily_request = Purchase::whereDay('dtmInsertedBy', $today)->get();
+        $monthly_request = Purchase::whereMonth('dtmInsertedBy', $today)->get();
+        $yearly_request = Purchase::whereMonth('dtmInsertedBy', $today)->get();
         return view('back.dashboard', [
-            'title' => 'Dashboard'
+            'title' => 'Dashboard',
+            'daily_request' => $daily_request,
+            'monthly_request' => $monthly_request,
+            'yearly_request' => $yearly_request,
         ]);
     }
 }
