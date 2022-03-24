7d79b650e5b766279092e6c9c76de9d5344a045a
budistwn15 <budi.setiawan112001@gmail.com>
2022-03-24 22:41:32 +0700

    update count dashboard

--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -2,14 +2,24 @@
 
 namespace App\Http\Controllers\Front;
 
-use App\Http\Controllers\Controller;
+use Carbon\Carbon;
 use App\Models\User;
+use App\Models\Purchase;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('welcome');
+        $today = Carbon::now();
+        $daily_request = Purchase::whereDay('dtmInsertedBy', $today)->get();
+        $monthly_request = Purchase::whereMonth('dtmInsertedBy', $today)->get();
+        $yearly_request = Purchase::whereMonth('dtmInsertedBy', $today)->get();
+        return view('welcome', [
+            'daily_request' => $daily_request,
+            'monthly_request' => $monthly_request,
+            'yearly_request' => $yearly_request,
+        ]);
     }
 }
