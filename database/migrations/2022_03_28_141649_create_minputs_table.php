869c364c8eaa7500bc36e82a5ab3a0793811b1e7
Budi Setiawan <budi.setiawan112001@gmail.com>
2022-05-25 12:07:59 +0700

    update migration inputs

--- database/migrations/2022_03_28_141649_create_minputs_table.php
+++ database/migrations/2022_03_28_141649_create_minputs_table.php
@@ -22,6 +22,7 @@ class CreateMinputsTable extends Migration
             $table->integer('intHarga');
             $table->integer('intSubTotal');
             $table->date('dtmTanggalKedatangan');
+            $table->string('txtStatus')->default('In Proccess');
             $table->dateTime('dtmInsertedBy');
             $table->dateTime('dtmUpdatedBy');
         });
