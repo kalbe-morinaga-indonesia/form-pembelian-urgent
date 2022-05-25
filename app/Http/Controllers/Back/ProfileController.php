99b719cb0c97d3c8fa7c90bafa4e8bc63d32af15
Budi Setiawan <budi.setiawan112001@gmail.com>
2022-05-25 12:07:19 +0700

    Update profile controller

--- app/Http/Controllers/Back/ProfileController.php
+++ app/Http/Controllers/Back/ProfileController.php
@@ -2,10 +2,35 @@
 
 namespace App\Http\Controllers\Back;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use RealRashid\SweetAlert\Facades\Alert;
+use Symfony\Component\HttpKernel\Profiler\Profile;
 
 class ProfileController extends Controller
 {
-    //
+    public function index()
+    {
+        $userid = Auth()->user()->id;
+        $user = User::where('id', $userid)->first();
+        return view('back.profile.index', [
+            'title' => 'Profile',
+            'user' => $user
+        ]);
+    }
+
+    public function update(Request $request)
+    {
+        User::where('id', Auth()->user()->id)->update([
+            'txtNama' => $request->txtNama,
+            'txtNoHp' => $request->txtNoHp,
+            'txtTempatLahir' => $request->txtTempatLahir,
+            'dtmTanggalLahir' => $request->dtmTanggalLahir,
+        ]);
+
+        Alert::success("Berhasil", "Profile $request->txtNama berhasil diubah");
+        return redirect()->route('profile');
+    }
 }
